b0d90838d5f24cad27baf5fafb7d84b855de3a9f
oanabaraian <oana.baraian@evozon.com>
2014-07-22 15:11:56 +0300

    VacationMenu

---
+++ src/main/java/com/pages/VacationMenu.java
@@ -0,0 +1,42 @@
+
+package com.pages;
+
+import net.thucydides.core.annotations.findby.FindBy;
+import net.thucydides.core.pages.PageObject;
+import net.thucydides.core.pages.WebElementFacade;
+
+//@DefaultUrl("http://en.wiktionary.org/wiki/Wiktionary:Main_Page")
+public class VacationMenu extends PageObject {
+
+    @FindBy(css="ul.nav-list li:nth-child(2) a")
+    private WebElementFacade newVacationLink;
+    
+    @FindBy(css="ul.nav-list li:nth-child(3) a")
+    private WebElementFacade myRequestsLink;
+
+    @FindBy(css="ul.nav-list li:nth-child(4) a")
+    private WebElementFacade myFreeDaysLink;
+    
+    @FindBy(css="ul.nav-list li:nth-child(5) a")
+    private WebElementFacade freeDaysHistoryLink;
+
+  
+    
+    public void click_newVacation() {
+    	element(newVacationLink).waitUntilVisible();
+    	newVacationLink.click();
+    }
+    
+    public void click_myRequests() {
+    	element(myRequestsLink).waitUntilVisible();
+    	myRequestsLink.click();
+    }
+    public void click_myFreeDays() {
+    	element(myFreeDaysLink).waitUntilVisible();
+    	myFreeDaysLink.click();
+    }
+    public void click_freeDaysHistory() {
+    	element(freeDaysHistoryLink).waitUntilVisible();
+    	freeDaysHistoryLink.click();
+    }
+}
